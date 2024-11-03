5efcf09ed23ef67378a6e9b587f0bac859cd7ad1
Joshua Villasol <143289948+Jvillaso@users.noreply.github.com>
2024-11-03 12:23:27 -0500

    updated code

--- E_Ink_Display.py
+++ E_Ink_Display.py
@@ -59,7 +59,7 @@ def main():
     
     #Icon List for weather Icons
     global font_weather_icons; font_weather_icons = ImageFont.truetype(working_directory + "/font/meteocons-webfont.ttf", 60)
-    global icons_list; icons_list = {u'01d':u'B', u'01n':u'C', u'02d':u'H', u'02n':u'I', u'03d':u'N', u'04d':u'Y', u'04n':u'Y', u'09d':u'R', u'09n':u'R', u'10d':u'R', u'10n':u'R', u'11d':u'P', u'11n':u'P', u'13d':u'G', u'13n':u'G', u'50d':u'M', u'50n':u'M'}
+    global icons_list; icons_list = {u'01d':u'B', u'01n':u'C', u'02d':u'H', u'02n':u'I', u'03d':u'N', u'03n':u'N', u'04d':u'Y', u'04n':u'Y', u'09d':u'R', u'09n':u'R', u'10d':u'R', u'10n':u'R', u'11d':u'P', u'11n':u'P', u'13d':u'G', u'13n':u'G', u'50d':u'M', u'50n':u'M'}
 
     #task font list
     global font_tasks_list_title; font_tasks_list_title = ImageFont.truetype(f"{working_directory}/font/Roboto-Light.ttf", 30)
@@ -126,7 +126,7 @@ def query_weather():
             break
 
         except ValueError:
-            print("== Weather API JSON Failed ++")
+            print("== Weather API JSON Failed ==")
             time.sleep(refresh_time)
 
 def refresh_Screen():
